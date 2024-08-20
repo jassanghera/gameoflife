7565796513c41189c02090cecc20de1b8f19a2c5
jassanghera <jaspreetsanghera95@gmail.com>
2024-08-20 15:56:46 -0400

    dead_state function
    dead_state function complete

--- gameoflife.py
+++ gameoflife.py
@@ -2,17 +2,21 @@ print("hello life")
 
 # dead_state should accept integer width and height
     #output: 2D grid as list of lists
-def dead_state(width, height)
+def dead_state(width, height):
 
     dead_board = []
 
-    for i in width:
-        dead_board.append([])
+    for i in range(height):
+        row = []
+        for j in range(width):
+            row.append(0)
+        dead_board.append(row)
+
 
     return dead_board
 
 
-print(dead_state(4,4))
+print(dead_state(4,5))
 
 
 
