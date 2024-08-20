13fbd0c7fbfb1e78d1a5116160f6d4a8dc7ad41d
jassanghera <jaspreetsanghera95@gmail.com>
2024-08-20 15:40:46 -0400

    first function test

--- gameoflife.py
+++ gameoflife.py
@@ -1,1 +1,18 @@
 print("hello life")
+
+# dead_state should accept integer width and height
+    #output: 2D grid as list of lists
+def dead_state(width, height)
+
+    dead_board = []
+
+    for i in width:
+        dead_board.append([])
+
+    return dead_board
+
+
+print(dead_state(4,4))
+
+
+
